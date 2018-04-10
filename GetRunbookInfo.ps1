cd0af0e1bb77680c2495c4ed5c27636f8ee1df5b
skreebydba <skreebydba@users.noreply.github.com>
2018-04-10 16:39:44 -0500

    All kinds of changes

--- GetRunbookInfo.ps1
+++ GetRunbookInfo.ps1
@@ -3,28 +3,23 @@ $rgname = 'fbgguggenheimrg';
 $autoacct = 'fbgautomation';
 $destacct = 'fbgjapan';
 $path = 'C:\automation\Runbooks';
-$runbook = @();
+$runbooks = @();
 
-
-$runbooks = Get-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationAccountName $autoacct;
+$runbooks = Get-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationAccountName $autoacct -Verbose;
 
 foreach($runbook in $runbooks)
 {
-    #$runbook;
-
     $export = Export-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationAccountName $autoacct -Name $runbook.Name -OutputFolder $path -Force;
-    $export;
-    $runbookinfo += @{Name=$runbook.Name; Type=$runbook.RunbookType; File=$export.name};
+    $runbook.RunbookType, $runbook.Name, $export.Name;
+    $runbookinfo += @{Name=$runbook.Name; Type=$runbook.RunbookType; File=$export.Name};
 }
-
-#$runbookinfo;
 
 foreach($info in $runbookinfo)
 {
-    #$info.Name, $info.Type, $info.File;
-    Import-AzureRMAutomationRunbook -Name $info.Name -Path $info.File `
-    -ResourceGroupName $rgname -AutomationAccountName $autoacct `
-    -Type $info.Type; 
+    $info.Name, $info.Type, $info.File;
+    #Import-AzureRMAutomationRunbook -Name $info.Name -Path $info.File `
+    #-ResourceGroupName $rgname -AutomationAccountName $autoacct `
+    #-Type $info.Type; 
 
 }
 #$files = (Get-ChildItem -Path $path).Name;
---
+++ GetRunbookInfo.txt
@@ -0,0 +1,33 @@
+﻿cls;
+$rgname = 'fbgguggenheimrg';
+$autoacct = 'fbgautomation';
+$destacct = 'fbgjapan';
+$path = 'C:\automation\Runbooks';
+$runbook = @();
+
+
+$runbooks = Get-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationAccountName $autoacct;
+
+foreach($runbook in $runbooks)
+{
+    #$runbook;
+
+    $export = Export-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationAccountName $autoacct -Name $runbook.Name -OutputFolder $path -Force;
+    $export;
+    $runbookinfo += @{Name=$runbook.Name; Type=$runbook.RunbookType; File=$export.name};
+}
+
+#$runbookinfo;
+
+foreach($info in $runbookinfo)
+{
+    #$info.Name, $info.Type, $info.File;
+    Import-AzureRMAutomationRunbook -Name $info.Name -Path $info.File `
+    -ResourceGroupName $rgname -AutomationAccountName $autoacct `
+    -Type $info.Type; 
+
+}
+#$files = (Get-ChildItem -Path $path).Name;
+
+#$files;
+
