48c558bda8115ec4a5eb62fba8b417676c9c83c1
skreebydba <skreebydba@users.noreply.github.com>
2018-04-10 16:51:41 -0500

    Added switch statement

--- GetRunbookInfo.ps1
+++ GetRunbookInfo.ps1
@@ -11,13 +11,18 @@ $runbooks = Get-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationA
 foreach($runbook in $runbooks)
 {
     $export = Export-AzureRmAutomationRunbook -ResourceGroupName $rgname -AutomationAccountName $autoacct -Name $runbook.Name -OutputFolder $path -Force;
-    $runbook.RunbookType, $runbook.Name, $export.Name;
+    $runbook.RunbookType;
     $runbookinfo += @{Name=$runbook.Name; Type=$runbook.RunbookType; File=$export.Name};
 }
 
 foreach($info in $runbookinfo)
 {
     $info.Name, $info.Type, $info.File;
+
+    switch($info.Type)
+    {
+        'Script' {'PowerShellWorkflow'}
+    }
     #Import-AzureRMAutomationRunbook -Name $info.Name -Path $info.File `
     #-ResourceGroupName $rgname -AutomationAccountName $autoacct `
     #-Type $info.Type; 
